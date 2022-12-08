05e8cbe7448392b926b059d337c6a8e7b9770f0e
Aritra Mandal <aritra.mandal2002@gmail.com>
2022-12-08 20:39:57 +0530

    Update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Project-Sheety
 #### Made with sheety API. An easier way to interact with google sheets.
-##### Feel free to use the Sheety class in your projects.
+##### Feel free to use the Sheety class inside sheety.py in your projects.
--- main.py
+++ main.py
@@ -3,23 +3,3 @@ from data_manager import Manager
 
 manager = Manager()
 manager.start()
-
-# sheet = Sheety(sheet_name='prices', url=sheety_url, token=TOKEN)
-# data = sheet.sheet_data
-# print(data)
-# city_names = sheet.get_column(col_name='city')
-# flight_search = FlightSearch()
-# print(flight_search.get_city_codes())
-# city_codes = flight_search.get_city_codes()
-# print(city_codes)
-# print(city_codes)
-
-# sheet.edit_row(args)
-# print(sheet.edit_row(row_id=2, row_params=dict(city='Paris', iataCode='', lowestPrice=50)))
-# sheet.get_sheet(args)
-# sheet.delete_row(2)
-# sheet.edit_value(arg)
-# sheet.edit_column(args)
-# print(sheet.get_row(row_index=4))
-# sheet.edit_column(col_name='iataCode', values=city_codes)
-# print(sheet.edit_value(row_id=6, col_name='lowestPrice', value=95))
